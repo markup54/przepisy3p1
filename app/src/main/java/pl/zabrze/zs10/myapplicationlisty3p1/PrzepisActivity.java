524c728ecfc4c800ec567b1fed30b8540ab4cf1a
markup54 <marzenakupka@tlen.pl>
2025-03-12 16:15:51 +0100

    iklad poziomy

--- app/src/main/java/pl/zabrze/zs10/myapplicationlisty3p1/PrzepisActivity.java
+++ app/src/main/java/pl/zabrze/zs10/myapplicationlisty3p1/PrzepisActivity.java
@@ -3,15 +3,31 @@ package pl.zabrze.zs10.myapplicationlisty3p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class PrzepisActivity extends AppCompatActivity {
     private TextView textView;
+    private  TextView textViewSkladniki;
+    private TextView textViewCzas;
+    private TextView textViewOsoby;
+    private RatingBar ratingBar;
+    private Button button;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_przepis);
         textView = findViewById(R.id.textView2);
+        textViewSkladniki = findViewById(R.id.textView3);
+        textViewCzas = findViewById(R.id.textView4);
+        textViewOsoby = findViewById(R.id.textView5);
+        button = findViewById(R.id.button2);
+        ratingBar = findViewById(R.id.ratingBar);
+
+
         String nazwaPrzepisu = getIntent().getExtras().getString(PrzepisyListaActivity.PRZEPIS);
         textView.setText(nazwaPrzepisu);
     }
